fa4e0465d427dc3db631e2d89c6f91ce647ddfb6
Francisco Martínez <fmartinez@pcpractico.es>
2016-12-05 22:43:46 +0100

    login control basic 1

--- RenovaTic/gestion/views.py
+++ RenovaTic/gestion/views.py
@@ -2,9 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from django.contrib.auth import views
+from django.shortcuts import redirect
+#from django.contrib.auth.models import User
 
 def index(request):
-    return HttpResponse("Software de gestion de renovaciones.<br> En desarrollo.")
+    #Comprobar si el usuario esta autentificado
+    if request.user.is_authenticated():
+        return HttpResponse("USUARIO AUTENFICIADO")
+    else:
+        return redirect('/login/')
+
+
+
+
 
 
 # Create your views here.

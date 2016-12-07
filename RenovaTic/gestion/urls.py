4202891957cda71cdfbeb69fcd2bd8dcb73fe70d
Francisco Martínez <fmartinez@pcpractico.es>
2016-12-07 23:04:55 +0100

    bootstrap intragrate

--- RenovaTic/gestion/urls.py
+++ RenovaTic/gestion/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^end_session/$', views.end_session, name='end_session'),
 
 ]
--- RenovaTic/gestion/views.py
+++ RenovaTic/gestion/views.py
@@ -3,15 +3,23 @@ from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from django.contrib.auth import views
 from django.shortcuts import redirect
+from django.contrib.auth import logout
+from django.template.response import TemplateResponse
 #from django.contrib.auth.models import User
 
 def index(request):
     #Comprobar si el usuario esta autentificado
     if request.user.is_authenticated():
-        return HttpResponse("USUARIO AUTENFICIADO")
+        texto = HttpResponse("USUARIO AUTENFICIADO: [<b>"+ request.user.username + "</b>] <a href='./end_session/'>SALIR</a>" )
+        #return texto
+        return  TemplateResponse (request, 'RenovaTic/dashboard.html')
+
     else:
         return redirect('/login/')
 
+def end_session(request):
+    logout(request)
+    return redirect('/login/')
 
 
 
